79271d0027340795aff08154abef454245611230
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-04-13 19:17:27 +0600

    commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,7 @@
 import "./Hero.css";
 import { HiLocationMarker } from "react-icons/hi";
+import CountUp from "react-countup";
+
 const Hero = () => {
   return (
     <section className="hero-wrapper">
@@ -11,13 +13,36 @@ const Hero = () => {
           <h1>Discover <br /> Most Suitable <br />Property</h1>
         </div>
         <div className="flexColStart hero-description">
-          <span>Fimd a varieties of property that suit you very easily.</span> <br />
+          <span>Find a varieties of property that suit you very easily.</span> <br />
           <span>Forget all difficulties to find a residence for you.</span>
         </div>
         <div className="flexCenter hero-search">
         <HiLocationMarker size={25} color="var(--blue)"/>
           <input type="text" name="search" id="search"/>
           <button className="button" >Search</button>
+        </div>
+        <div className="flexCenter stats">
+          <div className="flexColCenter stat">
+          <span>
+            <CountUp start={8800} end={9000} duration={4}/>
+            <span>+</span>
+          </span>
+            <span className="secondaryText">Premium Products</span>
+          </div>
+          <div className="flexColCenter stat">
+          <span>
+            <CountUp start={1950} end={2000} duration={4}/>
+            <span>+</span>
+          </span>
+            <span className="secondaryText">Happy Customers</span>
+          </div>
+          <div className="flexColCenter stat">
+          <span>
+            <CountUp end={28} duration={4}/>
+            <span>+</span>
+          </span>
+            <span className="secondaryText">Award Winning</span>
+          </div>
         </div>
         </div>
         {/* Right Side */}
